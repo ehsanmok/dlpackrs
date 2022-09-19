4100bff2e04e33e9079020042ac0b21f0ff80739
Ehsan M. Kermani <ehsan@tigera.io>
2022-09-18 20:24:02 -0700

    Clarify usage descriptions

--- src/lib.rs
+++ src/lib.rs
@@ -6,9 +6,9 @@
 //! <br>
 //!
 //! This crate provides a safe idiomatic Rust binding to [DLPack](https://dmlc.github.io/dlpack/latest/)
-//! which is the standard in-memory, (mostly) hardware agnosnic data exchange interface
-//! and is recognized by major Deep Learning frameworks such as [PyTorch](https://pytorch.org/docs/stable/dlpack.html),
-//! [Tensorflow](https://www.tensorflow.org/api_docs/python/tf/experimental/dlpack/from_dlpack),
+//! which is the standard in-memory, (mostly) hardware agnostic data format,
+//! recognized by major Deep Learning frameworks such as [PyTorch](https://pytorch.org/docs/stable/dlpack.html),
+//! [TensorFlow](https://www.tensorflow.org/api_docs/python/tf/experimental/dlpack/from_dlpack),
 //! [MXNet](https://mxnet.apache.org/versions/master/api/python/docs/_modules/mxnet/dlpack.html),
 //! [TVM](https://tvm.apache.org/docs/reference/api/python/contrib.html#module-tvm.contrib.dlpack)
 //! and major array processing frameworks such as [NumPy](https://numpy.org/doc/stable/release/1.22.0-notes.html#add-nep-47-compatible-dlpack-support)
@@ -19,26 +19,53 @@
 //!
 //! ## Usage
 //!
-//! There are two usages related to where the owner of the underlying data / storage of a tensor resides.
-//! The main distinction is when the underlying data gets dropped and which framework is responsible for it.
+//! There are two main cases related to where the owner of the underlying data / storage of a tensor resides and what kind of operations are to be done.
 //!
-//! ### Rust as the owner
+//! ### Memory Managed Tensor
 //!
-//!In this case, `ManagedTensor` is built from `ManagedTensorProxy` which is the same proxy for `ffi::DLManagedTensor`.
+//! In this case, `ManagedTensor` is built from `ManagedTensorProxy` which is a safe proxy for the unsafe `ffi::DLManagedTensor`.
 //!
-//! ### Non-Rust owner
+//! ### Non-Memory Manged Tensor (View)
 //!
-//! In this case, either `ffi::DLTensor` or its (invariant) Rust wrapper `Tensor` can be used.
+//! In this case, the (invariant) Rust wrapper `Tensor` can be used or if needed the unsafe `ffi::DLTensor`.
 //!
 //! <br>
 //!
 //! ## Example
 //!
-//! For when ownership is not concerned, one can use `Tensor`. Here is an example on how the conversion
+//! When ownership is concerned, one can use the `ManagedTensor`. Here is an example on how the bi-directional conversion
+//!
+//! <div align="center">ndarray::ArrayD <--> ManagedTensor</div>
+//!
+//! is done at zero-cost.
+//!
+//! ```no_run
+//! impl<'tensor, 'ctx> From<&'tensor mut ArrayD<f32>> for ManagedTensor<'tensor, 'ctx> {
+//!     fn from(t: &'tensor mut ArrayD<f32>) -> Self {
+//!         let dlt: Tensor<'tensor> = Tensor::from(t);
+//!         let inner = DLManagedTensor::new(dlt.0, ptr::null_mut());
+//!         ManagedTensor(inner)
+//!     }
+//! }
+//!
+//! impl<'tensor, 'ctx> From<&mut ManagedTensor<'tensor, 'ctx>> for ArrayD<f32> {
+//!     fn from(mt: &mut ManagedTensor<'tensor, 'ctx>) -> Self {
+//!         let dlt: DLTensor = mt.0.inner.dl_tensor.into();
+//!         unsafe {
+//!             let arr = RawArrayViewMut::from_shape_ptr(dlt.shape().unwrap(), dlt.data() as *mut f32);
+//!             arr.deref_into_view_mut().into_dyn().to_owned()
+//!         }
+//!     }
+//! }
+//! ```
+//!
+//! <br>
+//!
+//! And when ownership is not concerned, one can use `Tensor` as a view. Here is an example on how the bi-directional converion
 //!
 //! <div align="center">ndarray::ArrayD <--> Tensor</div>
 //!
-//! can happen at zero-cost.
+//! is done at zero-cost.
 //!
 //! ```no_run
 //! impl<'tensor> From<&'tensor mut ArrayD<f32>> for Tensor<'tensor> {
@@ -57,35 +84,9 @@
 //! }
 //!
 //! impl<'tensor> From<&'tensor mut Tensor<'tensor>> for ArrayD<f32> {
-//!    fn from(t: &'tensor mut Tensor<'tensor>) -> Self {
+//!     fn from(t: &'tensor mut Tensor<'tensor>) -> Self {
 //!         unsafe {
 //!             let arr = RawArrayViewMut::from_shape_ptr(t.0.shape().unwrap(), t.0.data() as *mut f32);
-//!             arr.deref_into_view_mut().into_dyn().to_owned()
-//!         }
-//!     }
-//! }
-//! ```
-//!
-//! And when ownership is concerned, one can use the `ManagedTensor`. Here is an example on how the conversion
-//!
-//! <div align="center">ndarray::ArrayD <--> ManagedTensor</div>
-//!
-//! can happen at zero-cost.
-//!
-//! ```no_run
-//! impl<'tensor, 'ctx> From<&'tensor mut ArrayD<f32>> for ManagedTensor<'tensor, 'ctx> {
-//!     fn from(t: &'tensor mut ArrayD<f32>) -> Self {
-//!         let dlt: Tensor<'tensor> = Tensor::from(t);
-//!         let inner = DLManagedTensor::new(dlt.0, ptr::null_mut());
-//!         ManagedTensor(inner)
-//!     }
-//! }
-//!
-//! impl<'tensor, 'ctx> From<&mut ManagedTensor<'tensor, 'ctx>> for ArrayD<f32> {
-//!     fn from(mt: &mut ManagedTensor<'tensor, 'ctx>) -> Self {
-//!         let dlt: DLTensor = mt.0.inner.dl_tensor.into();
-//!         unsafe {
-//!             let arr = RawArrayViewMut::from_shape_ptr(dlt.shape().unwrap(), dlt.data() as *mut f32);
 //!             arr.deref_into_view_mut().into_dyn().to_owned()
 //!         }
 //!     }
